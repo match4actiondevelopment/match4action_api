6c7a94d0e298d42bb78c52db707ae5910f0b553c
Joao Paulo <jprp17@live.com>
2022-12-24 16:39:15 -0300

    feat: setup express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+import { config } from 'dotenv';
+import express from 'express';
+
+config();
+const app = express();
+
+const port = process.env.PORT || 3003;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(port, () => console.log(`listening on port: ${port}`));
