ede15298d57083eadc96a4c5cddb0f4b449fb4cc
Joao Paulo <jprp17@live.com>
2022-12-25 17:19:21 -0300

    feat: create Initiative controller

---
+++ src/controllers/initiatives/create.ts
@@ -0,0 +1,40 @@
+import { InitiativeI } from '../../models/initiative';
+import { badRequest, created, serverError } from '../../utils/helpers';
+import { HttpRequest, HttpResponse, IController } from '../protocols';
+import { CreateInitiativeParams, ICreateInitiativeRepository } from './types';
+
+export class CreateInitiativeController implements IController {
+  constructor(private readonly createInitiativeRepository: ICreateInitiativeRepository) {}
+  async handle(httpRequest: HttpRequest<CreateInitiativeParams>): Promise<HttpResponse<InitiativeI | string>> {
+    try {
+      const requiredFields = [
+        'image',
+        'eventTimeFrame',
+        'eventType',
+        'name',
+        'moves',
+        'areas',
+        'services',
+        'description',
+        'startDate',
+        'endDate',
+        'postalCode',
+        'city',
+        'country',
+        'createdByUser',
+        'goals',
+      ];
+
+      for (const field of requiredFields) {
+        if (!httpRequest?.body?.[field as keyof CreateInitiativeParams]?.toString()?.length) {
+          return badRequest(`Field ${field} is required.`);
+        }
+      }
+
+      const initiative = await this.createInitiativeRepository.create(httpRequest.body!);
+      return created<InitiativeI>(initiative);
+    } catch (error) {
+      return serverError((error as Error).message);
+    }
+  }
+}
