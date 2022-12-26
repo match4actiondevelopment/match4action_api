d3b6836948a6a9fe250b4574acc6dd684d480609
Joao Paulo <jprp17@live.com>
2022-12-25 22:46:12 -0300

    feat: add imports correctly

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -2,3 +2,4 @@ export * from './create';
 export * from './delete';
 export * from './getAll';
 export * from './getOne';
+export * from './update';
--- src/repositories/initiatives/index.ts
+++ src/repositories/initiatives/index.ts
@@ -1,2 +1,4 @@
 export * from './create';
+export * from './delete';
 export * from './getAll';
+export * from './getOne';
