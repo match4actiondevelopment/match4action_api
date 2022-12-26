cd3bf93c84397509b928586a4a813beca64339de
Joao Paulo <jprp17@live.com>
2022-12-25 23:12:25 -0300

    refactor: move routes to its own file

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,8 @@
 import { config } from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import {
-  CreateGoalController,
-  DeleteGoalController,
-  GetGoalsController,
-  UpdateGoalController,
-} from './controllers/goals';
-import {
-  CreateInitiativeController,
-  DeleteInitiativeController,
-  GetInitiativeController,
-  GetInitiativesController,
-} from './controllers/initiatives';
-import {
-  CreateUserController,
-  DeleteUserController,
-  GetUserController,
-  GetUsersController,
-  UpdateUserController,
-} from './controllers/users';
-import {
-  CreateGoalRepository,
-  DeleteGoalRepository,
-  GetGoalsRepository,
-  UpdateGoalRepository,
-} from './repositories/goals';
-import {
-  CreateInitiativeRepository,
-  DeleteInitiativeRepository,
-  GetInitiativeRepository,
-  GetInitiativesRepository,
-} from './repositories/initiatives';
 
-import {
-  CreateUserRepository,
-  DeleteUserRepository,
-  GetUserRepository,
-  GetUsersRepository,
-  UpdateUserRepository,
-} from './repositories/users';
+import router from './router';
 
 config();
 
@@ -70,97 +33,9 @@ mongoose
     const app = express();
 
     app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-    app.get('/users', async (req, res) => {
-      const getUsersRepository = new GetUsersRepository();
-      const getUsersControllers = new GetUsersController(getUsersRepository);
-      const { body, statusCode } = await getUsersControllers.handle();
-      res.status(statusCode).send(body);
-    });
-
-    app.post('/users', async (req, res) => {
-      const createUserRepository = new CreateUserRepository();
-      const createUserControllers = new CreateUserController(createUserRepository);
-      const { body, statusCode } = await createUserControllers.handle({ body: req.body });
-      res.status(statusCode).send(body);
-    });
-
-    app.get('/users/:id', async (req, res) => {
-      const getUserRepository = new GetUserRepository();
-      const getUserControllers = new GetUserController(getUserRepository);
-      const { body, statusCode } = await getUserControllers.handle({ params: req.params });
-      res.status(statusCode).send(body);
-    });
-
-    app.put('/users/:id', async (req, res) => {
-      const updateUserRepository = new UpdateUserRepository();
-      const updateUserControllers = new UpdateUserController(updateUserRepository);
-      const { body, statusCode } = await updateUserControllers.handle({ body: req.body, params: req.params });
-      res.status(statusCode).send(body);
-    });
-
-    app.delete('/users/:id', async (req, res) => {
-      const deleteUserRepository = new DeleteUserRepository();
-      const deleteUserControllers = new DeleteUserController(deleteUserRepository);
-      const { body, statusCode } = await deleteUserControllers.handle({ params: req.params });
-      res.status(statusCode).send(body);
-    });
-
-    app.get('/goals', async (req, res) => {
-      const getGoalsRepository = new GetGoalsRepository();
-      const getGoalsController = new GetGoalsController(getGoalsRepository);
-      const { body, statusCode } = await getGoalsController.handle();
-      res.status(statusCode).send(body);
-    });
-
-    app.post('/goals', async (req, res) => {
-      const createGoalRepository = new CreateGoalRepository();
-      const createGoalControllers = new CreateGoalController(createGoalRepository);
-      const { body, statusCode } = await createGoalControllers.handle({ body: req.body });
-      res.status(statusCode).send(body);
-    });
-
-    app.put('/goals/:id', async (req, res) => {
-      const updateGoalRepository = new UpdateGoalRepository();
-      const updateGoalController = new UpdateGoalController(updateGoalRepository);
-      const { body, statusCode } = await updateGoalController.handle({ body: req.body, params: req.params });
-      res.status(statusCode).send(body);
-    });
-
-    app.delete('/goals/:id', async (req, res) => {
-      const deleteGoalRepository = new DeleteGoalRepository();
-      const deleteGoalController = new DeleteGoalController(deleteGoalRepository);
-      const { body, statusCode } = await deleteGoalController.handle({ params: req.params });
-      res.status(statusCode).send(body);
-    });
-
-    app.get('/initiatives', async (req, res) => {
-      const getInitiativesRepository = new GetInitiativesRepository();
-      const getInitiativesControllers = new GetInitiativesController(getInitiativesRepository);
-      const { body, statusCode } = await getInitiativesControllers.handle();
-      res.status(statusCode).send(body);
-    });
-
-    app.post('/initiatives', async (req, res) => {
-      const createInitiativeRepository = new CreateInitiativeRepository();
-      const createInitiativeControllers = new CreateInitiativeController(createInitiativeRepository);
-      const { body, statusCode } = await createInitiativeControllers.handle({ body: req.body });
-      res.status(statusCode).send(body);
-    });
-
-    app.get('/initiatives/:id', async (req, res) => {
-      const getInitiativeRepository = new GetInitiativeRepository();
-      const getInitiativesControllers = new GetInitiativeController(getInitiativeRepository);
-      const { body, statusCode } = await getInitiativesControllers.handle({ params: req.params });
-      res.status(statusCode).send(body);
-    });
-
-    app.delete('/initiatives/:id', async (req, res) => {
-      const getInitiativeRepository = new DeleteInitiativeRepository();
-      const getInitiativesControllers = new DeleteInitiativeController(getInitiativeRepository);
-      const { body, statusCode } = await getInitiativesControllers.handle({ params: req.params });
-      res.status(statusCode).send(body);
-    });
+    app.use('/api', router);
 
     app.listen(port, () => {
       console.log(`server is running on: http://localhost:${port}`);
---
+++ src/router.ts
@@ -0,0 +1,151 @@
+import { Router } from 'express';
+import {
+  CreateGoalController,
+  DeleteGoalController,
+  GetGoalsController,
+  UpdateGoalController,
+} from './controllers/goals';
+import {
+  CreateInitiativeController,
+  DeleteInitiativeController,
+  GetInitiativeController,
+  GetInitiativesController,
+} from './controllers/initiatives';
+import {
+  CreateUserController,
+  DeleteUserController,
+  GetUserController,
+  GetUsersController,
+  UpdateUserController,
+} from './controllers/users';
+import {
+  CreateGoalRepository,
+  DeleteGoalRepository,
+  GetGoalsRepository,
+  UpdateGoalRepository,
+} from './repositories/goals';
+import {
+  CreateInitiativeRepository,
+  DeleteInitiativeRepository,
+  GetInitiativeRepository,
+  GetInitiativesRepository,
+} from './repositories/initiatives';
+import {
+  CreateUserRepository,
+  DeleteUserRepository,
+  GetUserRepository,
+  GetUsersRepository,
+  UpdateUserRepository,
+} from './repositories/users';
+
+const router = Router();
+
+/*
+  repositories
+*/
+const getUsersRepository = new GetUsersRepository();
+const createUserRepository = new CreateUserRepository();
+const getUserRepository = new GetUserRepository();
+const updateUserRepository = new UpdateUserRepository();
+const deleteUserRepository = new DeleteUserRepository();
+const getGoalsRepository = new GetGoalsRepository();
+const createGoalRepository = new CreateGoalRepository();
+const deleteGoalRepository = new DeleteGoalRepository();
+const updateGoalRepository = new UpdateGoalRepository();
+const getInitiativesRepository = new GetInitiativesRepository();
+const getInitiativeRepository = new GetInitiativeRepository();
+const createInitiativeRepository = new CreateInitiativeRepository();
+const deleteInitiativeRepository = new DeleteInitiativeRepository();
+
+/*
+  controller
+*/
+const getUsersControllers = new GetUsersController(getUsersRepository);
+const createUserControllers = new CreateUserController(createUserRepository);
+const getUserControllers = new GetUserController(getUserRepository);
+const updateUserControllers = new UpdateUserController(updateUserRepository);
+const deleteUserControllers = new DeleteUserController(deleteUserRepository);
+const getGoalsController = new GetGoalsController(getGoalsRepository);
+const createGoalControllers = new CreateGoalController(createGoalRepository);
+const deleteGoalController = new DeleteGoalController(deleteGoalRepository);
+const updateGoalController = new UpdateGoalController(updateGoalRepository);
+const getInitiativesControllers = new GetInitiativesController(getInitiativesRepository);
+const createInitiativeControllers = new CreateInitiativeController(createInitiativeRepository);
+const getInitiativeController = new GetInitiativeController(getInitiativeRepository);
+const deleteInitiativeControllers = new DeleteInitiativeController(deleteInitiativeRepository);
+
+/*
+  users
+*/
+router.get('/users', async (req, res) => {
+  const { body, statusCode } = await getUsersControllers.handle();
+  res.status(statusCode).send(body);
+});
+
+router.post('/users', async (req, res) => {
+  const { body, statusCode } = await createUserControllers.handle({ body: req.body });
+  res.status(statusCode).send(body);
+});
+
+router.get('/users/:id', async (req, res) => {
+  const { body, statusCode } = await getUserControllers.handle({ params: req.params });
+  res.status(statusCode).send(body);
+});
+
+router.put('/users/:id', async (req, res) => {
+  const { body, statusCode } = await updateUserControllers.handle({ body: req.body, params: req.params });
+  res.status(statusCode).send(body);
+});
+
+router.delete('/users/:id', async (req, res) => {
+  const { body, statusCode } = await deleteUserControllers.handle({ params: req.params });
+  res.status(statusCode).send(body);
+});
+
+/*
+  goals
+*/
+router.get('/goals', async (req, res) => {
+  const { body, statusCode } = await getGoalsController.handle();
+  res.status(statusCode).send(body);
+});
+
+router.post('/goals', async (req, res) => {
+  const { body, statusCode } = await createGoalControllers.handle({ body: req.body });
+  res.status(statusCode).send(body);
+});
+
+router.put('/goals/:id', async (req, res) => {
+  const { body, statusCode } = await updateGoalController.handle({ body: req.body, params: req.params });
+  res.status(statusCode).send(body);
+});
+
+router.delete('/goals/:id', async (req, res) => {
+  const { body, statusCode } = await deleteGoalController.handle({ params: req.params });
+  res.status(statusCode).send(body);
+});
+
+/*
+  initiatives
+*/
+router.get('/initiatives', async (req, res) => {
+  const { body, statusCode } = await getInitiativesControllers.handle();
+  res.status(statusCode).send(body);
+});
+
+router.post('/initiatives', async (req, res) => {
+  const { body, statusCode } = await createInitiativeControllers.handle({ body: req.body });
+  res.status(statusCode).send(body);
+});
+
+router.get('/initiatives/:id', async (req, res) => {
+  const { body, statusCode } = await getInitiativeController.handle({ params: req.params });
+  res.status(statusCode).send(body);
+});
+
+router.delete('/initiatives/:id', async (req, res) => {
+  const { body, statusCode } = await deleteInitiativeControllers.handle({ params: req.params });
+  res.status(statusCode).send(body);
+});
+
+export default router;
