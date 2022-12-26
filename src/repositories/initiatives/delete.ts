1b9044a2e67888dfae50ef563aaf495491d7800e
Joao Paulo <jprp17@live.com>
2022-12-25 22:16:33 -0300

    feat: delete Initiative controller

---
+++ src/repositories/initiatives/delete.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import { IDeleteInitiativeRepository } from '../../controllers/initiatives/types';
+import { Initiative } from '../../models/initiative';
+
+export class DeleteInitiativeRepository implements IDeleteInitiativeRepository {
+  async delete(id: string): Promise<{ success: boolean }> {
+    const objectId = new mongoose.Types.ObjectId(id);
+
+    const initiative = await Initiative.findById(objectId);
+
+    if (!initiative) {
+      throw new Error('Initiative not found.');
+    }
+
+    const deletedInitiative = await Initiative.findOneAndDelete(objectId);
+
+    return { success: !!deletedInitiative };
+  }
+}
