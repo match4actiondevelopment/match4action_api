7e37e32f5998860a4beff689a17c3a7a02d7d907
Joao Paulo <jprp17@live.com>
2023-01-09 00:09:56 -0300

    feat: create generateTokens util function

---
+++ src/utils/generateToken.ts
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import { UserRole } from '../models/User';
+import { UserToken } from '../models/UserToken';
+import { ACCESS_TOKEN_PRIVATE_KEY, REFRESH_TOKEN_PRIVATE_KEY } from './constants';
+
+export interface GenerateTokensInterface {
+  _id: string;
+  email: string;
+  role: UserRole;
+}
+
+export const generateTokens = async (user: GenerateTokensInterface) => {
+  try {
+    const payload = { _id: user._id, role: user.role, email: user.email };
+    const access_token = jwt.sign(payload, ACCESS_TOKEN_PRIVATE_KEY, { expiresIn: '14m' });
+    const refresh_token = jwt.sign(payload, REFRESH_TOKEN_PRIVATE_KEY, { expiresIn: '30d' });
+
+    const userToken = await UserToken.findOne({ userId: user._id });
+
+    if (userToken) await userToken.remove();
+
+    await new UserToken({ userId: user._id, refresh_token }).save();
+    return Promise.resolve({ access_token, refresh_token });
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
