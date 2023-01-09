0dad292e450570325dbca4c7c9055d0b713c8e25
Joao Paulo <jprp17@live.com>
2023-01-09 00:10:33 -0300

    feat: create verifyRefreshToken util function

---
+++ src/utils/verifyRefreshToken.ts
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { UserToken } from '../models/UserToken';
+import { REFRESH_TOKEN_PRIVATE_KEY } from './constants';
+
+export const verifyRefreshToken = (refreshToken: string) => {
+  return new Promise((resolve, reject) => {
+    UserToken.findOne({ access_token: refreshToken }, (err: any, doc: any) => {
+      if (!doc) return reject({ error: true, message: 'Invalid refresh token.' });
+
+      jwt.verify(refreshToken, REFRESH_TOKEN_PRIVATE_KEY, (err, tokenDetails) => {
+        if (err) return reject({ error: true, message: 'Invalid refresh token.' });
+        resolve({
+          tokenDetails,
+          error: false,
+          message: 'Valid refresh token.',
+        });
+      });
+    });
+  });
+};
