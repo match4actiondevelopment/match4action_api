5c1dee749152a50b32e227c3d0b90bc269aeeeda
Joao Paulo <jprp17@live.com>
2023-01-09 00:48:31 -0300

    feat: update the verifyRefreshToken types

--- src/utils/verifyRefreshToken.ts
+++ src/utils/verifyRefreshToken.ts
@@ -1,6 +1,21 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JsonWebTokenError, NotBeforeError, TokenExpiredError } from 'jsonwebtoken';
+import { UserRole } from '../models/User';
 import { UserToken } from '../models/UserToken';
 import { REFRESH_TOKEN_PRIVATE_KEY } from './constants';
+
+export interface UserJWTPayload {
+  _id: string;
+  role: UserRole;
+  email: string;
+  iat: number;
+  exp: number;
+}
+
+export interface VerifyRefreshTokenSuccessResponse {
+  tokenDetails: UserJWTPayload;
+  success: boolean;
+  message: string;
+}
 
 export const verifyRefreshToken = (refreshToken: string) => {
   return new Promise((resolve, reject) => {
@@ -8,12 +23,31 @@ export const verifyRefreshToken = (refreshToken: string) => {
       if (!doc) return reject({ error: true, message: 'Invalid refresh token.' });
 
       jwt.verify(refreshToken, REFRESH_TOKEN_PRIVATE_KEY, (err, tokenDetails) => {
-        if (err) return reject({ error: true, message: 'Invalid refresh token.' });
-        resolve({
-          tokenDetails,
-          error: false,
-          message: 'Valid refresh token.',
-        });
+        if (err instanceof TokenExpiredError) {
+          reject({
+            success: false,
+            message: 'Unauthorized! Access Token was expired!',
+          });
+        }
+        if (err instanceof NotBeforeError) {
+          reject({
+            success: false,
+            message: 'Jwt not active,',
+          });
+        }
+        if (err instanceof JsonWebTokenError) {
+          reject({
+            success: false,
+            message: 'Jwt malformed.',
+          });
+        }
+
+        if (tokenDetails)
+          resolve({
+            tokenDetails,
+            success: true,
+            message: 'Valid refresh token.',
+          });
       });
     });
   });
