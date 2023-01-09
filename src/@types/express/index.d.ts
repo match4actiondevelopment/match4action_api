3450734eb309f3f3641c0426e903323fc2de94e5
Joao Paulo <jprp17@live.com>
2023-01-09 00:46:58 -0300

    fix: update the user payload returned from http response

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,7 +1,11 @@
-declare global {
-  namespace Express {
-    interface Request {
-      user: any;
-    }
+declare module Express {
+  export interface Request {
+    user: {
+      _id: string;
+      role: UserRole;
+      email: string;
+      iat: number;
+      exp: number;
+    };
   }
 }
