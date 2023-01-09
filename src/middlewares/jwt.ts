b78bab021ed8272a2452c24936a3208f865b5505
Joao Paulo <jprp17@live.com>
2023-01-09 00:49:02 -0300

    feat: update the jwt middleware

--- src/middlewares/jwt.ts
+++ src/middlewares/jwt.ts
@@ -1,31 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
-import jwt, { JsonWebTokenError, NotBeforeError, TokenExpiredError } from 'jsonwebtoken';
-import { UserInterface } from '../models/User';
+import { UserRole } from '../models/User';
+import { verifyRefreshToken, VerifyRefreshTokenSuccessResponse } from '../utils/verifyRefreshToken';
 
-const jwtSecret: string = process.env.JWT_SECRET ?? 'asddd3232sa123';
-
-export const generateToken = (data?: Express.User) => {
-  const token = jwt.sign(
-    {
-      id: (data as any)._id,
-      role: (data as any).role,
-      name: (data as any).name,
-    },
-    jwtSecret,
-    { expiresIn: process.env.TOKEN_LIFE }
-  );
-  return token;
-};
-
-export interface TokenData {
-  id: string;
-  role: string;
-  name: string;
-  iat: number;
-  exp: number;
-}
-
-export const authGuard = (req: Request, res: Response, next: NextFunction) => {
+export const authGuard = async (req: Request, res: Response, next: NextFunction) => {
   const bearer = req.headers.authorization;
 
   if (!bearer) {
@@ -39,29 +16,11 @@ export const authGuard = (req: Request, res: Response, next: NextFunction) => {
   }
 
   try {
-    const decoded = jwt.verify(token, jwtSecret, (err, decoded) => {
-      if (err instanceof TokenExpiredError) {
-        return res.status(401).send({
-          success: false,
-          message: 'Unauthorized! Access Token was expired!',
-        });
-      }
-      if (err instanceof NotBeforeError) {
-        return res.status(401).send({
-          success: false,
-          message: 'Jwt not active,',
-        });
-      }
-      if (err instanceof JsonWebTokenError) {
-        return res.status(401).send({
-          success: false,
-          message: 'Jwt malformed.',
-        });
-      }
-      return decoded;
-    });
+    const decoded = (await verifyRefreshToken(token)) as VerifyRefreshTokenSuccessResponse;
 
-    req.user = decoded as unknown as TokenData;
+    if (decoded.success) {
+      req.user = decoded.tokenDetails;
+    }
 
     next();
   } catch (e) {
@@ -74,9 +33,7 @@ export const authGuard = (req: Request, res: Response, next: NextFunction) => {
 };
 
 export const adminGuard = (req: Request, res: Response, next: NextFunction) => {
-  const user = req?.user as UserInterface;
-
-  if (req?.user && user?.role !== 'user') {
+  if (req?.user && req?.user?.role !== UserRole.admin) {
     res.status(401).json({ success: false, message: 'Unauthorized!' });
   } else {
     next();
