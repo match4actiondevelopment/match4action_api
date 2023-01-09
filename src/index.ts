e22be0544bdad3587fd78315783a991a0d97f190
Joao Paulo <jprp17@live.com>
2023-01-09 00:09:11 -0300

    feat: add user Express custom payload from req.user

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import cors from 'cors';
 import { config } from 'dotenv';
 import express from 'express';
-import session from 'express-session';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
-import passport from 'passport';
 import { errorHandler } from './middlewares/errorHandler';
 import { authRouter } from './routes/auth';
 import { usersRouter } from './routes/users';
@@ -15,12 +13,6 @@ const url = process.env.MONGODB_URL ?? '';
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 const port = process.env.PORT ?? 3003;
-const sessionSecret = process.env.SESSION_SECRET ?? 'secretSession';
-
-// Passport config
-import { passportStrategy } from './config/passport';
-
-passportStrategy(passport);
 
 const app = express();
 
@@ -28,20 +20,12 @@ app.use(morgan('dev'));
 app.use(
   cors({
     origin: 'http://localhost:3000',
-    methods: 'GET,POST,PUT,DELETE',
+    methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true,
   })
 );
 
 mongoose.set('strictQuery', false);
-
-mongoose.set('toJSON', {
-  virtuals: true,
-  transform: (_, converted) => {
-    delete converted.__v;
-    delete converted._id;
-  },
-});
 
 // Connect to Mongo
 mongoose
@@ -59,37 +43,6 @@ mongoose
 // Allow express to parse JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// Express Session
-app.use(
-  session({
-    name: 'session',
-    secret: sessionSecret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  })
-);
-
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user: any, cb) => {
-  process.nextTick(() => {
-    return cb(null, {
-      id: user._id,
-      username: user.username,
-      picture: user.picture,
-    });
-  });
-});
-
-passport.deserializeUser((user: any, cb) => {
-  process.nextTick(() => {
-    return cb(null, user);
-  });
-});
 
 // Routes
 app.use('/auth', authRouter);
