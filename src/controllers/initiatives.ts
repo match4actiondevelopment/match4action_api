3589a4a5fcd6b782e3d428448735978e2ef968f6
Joao Paulo <jprp17@live.com>
2023-03-06 13:21:38 -0300

    feat: create initiative controller and route

--- src/controllers/initiatives.ts
+++ src/controllers/initiatives.ts
@@ -1,7 +1,10 @@
-import { Request, Response } from 'express';
-import mongoose from 'mongoose';
-import { Initiative, InitiativeDocument } from '../models/Initiatives';
-import { User } from '../models/User';
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+import { Initiative, InitiativeDocument } from "../models/Initiatives";
+import { User } from "../models/User";
+import { UploadController } from "./upload";
+
+const uploadController = new UploadController();
 
 export class InitiativesController {
   async getAll(req: Request, res: Response): Promise<void> {
@@ -20,12 +23,12 @@ export class InitiativesController {
   async getOne(req: Request, res: Response): Promise<void> {
     try {
       if (!req.params.id) {
-        throw new Error('Id is required.');
+        throw new Error("Id is required.");
       }
 
       const initiative = await Initiative.findById(req.params.id)
-        .populate('goals', 'name image')
-        .populate('userId', 'name');
+        .populate("goals", "name image")
+        .populate("userId", "name");
 
       res.status(200).json({
         success: true,
@@ -39,32 +42,53 @@ export class InitiativesController {
   async create(req: Request, res: Response): Promise<void> {
     try {
       const userLogged = req.user;
-      const user = await User.findById(req.params.id);
+      const user = await User.findById(userLogged?._id);
 
       if (!user) {
-        res.status(404).json({ success: false, message: 'User not found.' });
+        res.status(404).json({ success: false, message: "User not found." });
         return;
       }
 
       if (userLogged?._id?.toString() !== user?._id?.toString()) {
-        res.status(401).json({ success: false, message: 'Unauthorized! Access Token invalid!' });
+        res.status(401).json({
+          success: false,
+          message: "Unauthorized! Access Token invalid!",
+        });
         return;
       }
 
       const endTime = new Date();
-      const [endTimeHour, endTimeMinutes] = req.body.endTime.split(':');
+      const [endTimeHour, endTimeMinutes] = req?.body?.endTime.split(":");
       endTime.setHours(endTimeHour);
       endTime.setMinutes(endTimeMinutes);
 
       const startTime = new Date();
-      const [startTimeHour, startTimeMinutes] = req.body.startTime.split(':');
+      const [startTimeHour, startTimeMinutes] = req?.body?.startTime.split(":");
       startTime.setHours(startTimeHour);
       startTime.setMinutes(startTimeMinutes);
+
+      let image = null;
+      if (req?.file) {
+        image = await uploadController.create(req, res);
+
+        if (!image?.success) {
+          throw new Error("Error: image not uploaded.");
+        }
+      }
 
       const initiative = new Initiative({
         ...req.body,
         startTime,
         endTime,
+        image: image?.data ?? null,
+        userId: req.body.id,
+        whatMovesThisInitiative: JSON.parse(req.body.whatMovesThisInitiative),
+        servicesNeeded: JSON.parse(req.body.servicesNeeded),
+        whichAreasAreCoveredByThisInitiative: JSON.parse(
+          req.body.whichAreasAreCoveredByThisInitiative
+        ),
+        goals: JSON.parse(req.body.goals),
+        location: JSON.parse(req.body.location),
       });
 
       const { _id } = await initiative.save();
@@ -72,7 +96,7 @@ export class InitiativesController {
       const createdInitiative = await Initiative.findById(_id);
 
       if (!createdInitiative) {
-        throw new Error('Initiative not created.');
+        throw new Error("Initiative not created.");
       }
 
       res.status(201).json({
@@ -89,7 +113,7 @@ export class InitiativesController {
       const initiative = await Initiative.findById(req.params.id);
 
       if (!initiative) {
-        throw new Error('Initiative not found.');
+        throw new Error("Initiative not found.");
       }
 
       const objectId = new mongoose.Types.ObjectId(req.params.id);
@@ -103,30 +127,46 @@ export class InitiativesController {
     }
   }
 
-  async subscribe(req: Request, res: Response): Promise<InitiativeDocument | any> {
+  async subscribe(
+    req: Request,
+    res: Response
+  ): Promise<InitiativeDocument | any> {
     try {
       const userLogged = req.user;
 
       if (!userLogged) {
-        return res.status(404).json({ success: false, message: 'User not found.' });
+        return res
+          .status(404)
+          .json({ success: false, message: "User not found." });
       }
 
       const initiative = await Initiative.findById(req.params.id);
 
       if (!initiative) {
-        return res.status(404).json({ success: false, message: 'Initiative not found.' });
+        return res
+          .status(404)
+          .json({ success: false, message: "Initiative not found." });
       }
 
       const updateInitiative = new Initiative(initiative);
-      updateInitiative.applicants = [...updateInitiative.applicants, req.user?._id];
+      updateInitiative.applicants = [
+        ...updateInitiative.applicants,
+        req.user?._id,
+      ];
 
-      const newInitiative = await User.findByIdAndUpdate(req.params.id, updateInitiative, {
-        upsert: true,
-        returnOriginal: false,
-      });
+      const newInitiative = await User.findByIdAndUpdate(
+        req.params.id,
+        updateInitiative,
+        {
+          upsert: true,
+          returnOriginal: false,
+        }
+      );
 
       if (!newInitiative) {
-        return res.status(404).json({ success: false, message: 'Initiative not updated.' });
+        return res
+          .status(404)
+          .json({ success: false, message: "Initiative not updated." });
       }
 
       res.status(200).json({ success: true, data: newInitiative });
--- src/routes/initiatives.ts
+++ src/routes/initiatives.ts
@@ -1,15 +1,16 @@
-import { Router } from 'express';
-import { InitiativesController } from '../controllers/initiatives';
-import { authGuard } from '../utils/authGuard';
+import { Router } from "express";
+import { InitiativesController } from "../controllers/initiatives";
+import { authGuard } from "../utils/authGuard";
+import { multerUpload } from "../utils/multer";
 
 const router: Router = Router();
 
 const initiatives = new InitiativesController();
 
-router.get('/', initiatives.getAll);
-router.post('/', authGuard, initiatives.create);
-router.get('/:id', authGuard, initiatives.getOne);
-router.delete('/:id', authGuard, initiatives.delete);
-router.patch('/:id', authGuard, initiatives.subscribe);
+router.get("/", initiatives.getAll);
+router.post("/", authGuard, multerUpload.single("file"), initiatives.create);
+router.get("/:id", authGuard, initiatives.getOne);
+router.delete("/:id", authGuard, initiatives.delete);
+router.patch("/:id", authGuard, initiatives.subscribe);
 
 export { router as initiativesRouter };
