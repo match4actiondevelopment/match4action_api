e43a9d5dbc460c1e5a012f120c860bbc6c2fddf2
Joao Paulo <jprp17@live.com>
2023-03-07 09:28:51 -0300

    fix: add correct cors urls

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,30 @@
-import cookieSession from 'cookie-session';
-import cors from 'cors';
-import express from 'express';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import passport from 'passport';
-import './config/passport';
-import { authRoutes } from './routes/auth';
-import { goalsRouter } from './routes/goals';
-import { initiativesRouter } from './routes/initiatives';
-import { uploadRouter } from './routes/upload';
-import { usersRouter } from './routes/user';
-import { COOKIE_KEY, MONGO_URI, PORT } from './utils/secrets';
+import cookieSession from "cookie-session";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import passport from "passport";
+import "./config/passport";
+import { authRoutes } from "./routes/auth";
+import { goalsRouter } from "./routes/goals";
+import { initiativesRouter } from "./routes/initiatives";
+import { uploadRouter } from "./routes/upload";
+import { usersRouter } from "./routes/user";
+import { COOKIE_KEY, MONGO_URI, PORT } from "./utils/secrets";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://match4action-api.onrender.com'],
-    methods: 'GET,POST,PUT,DELETE,PATCH',
+    origin: [
+      "http://localhost:3000",
+      "http://match4action-api.onrender.com",
+      "https://match4action-api.onrender.com",
+    ],
     credentials: true,
   })
 );
@@ -36,24 +39,24 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 mongoose.connect(MONGO_URI, () => {
-  console.log('connected to mongodb');
+  console.log("connected to mongodb");
 });
 
-app.use('/auth', authRoutes);
-app.use('/users', usersRouter);
-app.use('/upload', uploadRouter);
-app.use('/goals', goalsRouter);
-app.use('/initiatives', initiativesRouter);
+app.use("/auth", authRoutes);
+app.use("/users", usersRouter);
+app.use("/upload", uploadRouter);
+app.use("/goals", goalsRouter);
+app.use("/initiatives", initiativesRouter);
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   const err = new Error(`Route ${req.originalUrl} not found`) as any;
   err.statusCode = 404;
   next(err);
 });
 
 app.listen(PORT, () => {
-  console.log('App listening on port: ' + PORT);
+  console.log("App listening on port: " + PORT);
 });
