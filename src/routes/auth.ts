10525733e09b69a3c321a94b82f39dffb692693e
Joao Paulo <jprp17@live.com>
2023-03-19 02:29:16 -0300

    refact: update auth routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,42 +1,19 @@
 import { Router } from "express";
 import passport from "passport";
-import { AuthController } from "../controllers/auth";
-import { UserDocument } from "../models/User";
-import { UserToken } from "../models/UserToken";
-import { authGuard } from "../utils/authGuard";
-import { CLIENT_BASE_URL } from "../utils/secrets";
+import {
+  google,
+  login,
+  logout,
+  refreshToken,
+  register,
+} from "../controllers/auth";
 
 const router: Router = Router();
-const auth = new AuthController();
 
-router.post("/login", auth.signIn);
-router.post("/refreshToken", auth.refreshToken);
-
-router.post("/logout", authGuard, async function (req, res, next) {
-  try {
-    await UserToken.deleteMany({ userId: req.user?._id });
-    req.logOut((err) => {
-      if (err) {
-        return next(err);
-      }
-    });
-  } catch (e) {
-    console.error(e);
-  }
-  res.status(200).json({ success: req.isAuthenticated() });
-});
-
-router.post(
-  "/register",
-  passport.authenticate("register"),
-  async (req, res) => {
-    const user = req.user as UserDocument;
-    const response = await auth.signUp(user);
-
-    res.status(201).json(response);
-  }
-);
-
+router.post("/login", login);
+router.post("/register", register);
+router.post("/logout", logout);
+router.post("/refreshToken", refreshToken);
 router.get(
   "/google",
   passport.authenticate("google", {
@@ -44,22 +21,6 @@ router.get(
   })
 );
 
-router.get(
-  "/google/redirect",
-  passport.authenticate("google"),
-  async (req, res) => {
-    const response = await auth.googleSignIn(req?.user?.email);
+router.get("/google/redirect", passport.authenticate("google"), google);
 
-    if (response?.success) {
-      let url = new URL(CLIENT_BASE_URL);
-      url.searchParams.set("access_token", response?.data?.access_token);
-      url.searchParams.set("refresh_token", response?.data?.refresh_token);
-      url.searchParams.set("user_id", response?.data?.userId);
-      res.redirect(url.toString());
-    } else {
-      res.redirect(`${CLIENT_BASE_URL}/404`);
-    }
-  }
-);
-
-export { router as authRoutes };
+export { router as auth };
