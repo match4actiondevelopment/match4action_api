934a07d651df893f693bb910861e414b3df8f34d
Joao Paulo <jprp17@live.com>
2023-03-19 02:32:14 -0300

    refact: update user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,118 +1,129 @@
-import { Request, Response } from 'express';
-import mongoose, { isValidObjectId } from 'mongoose';
-import { User, UserDocument } from '../models/User';
+import { NextFunction, Request, Response } from "express";
+import { User } from "../models/User";
+import { createError } from "../utils/createError";
 
-export class UserController {
-  async getAll(req: Request, res: Response): Promise<UserDocument[] | any> {
-    try {
-      const users = await User.find().select('-password');
+export const getAll = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find().select("-password");
 
-      res.status(200).json({ data: users });
-    } catch (error) {
-      res.status(404);
-    }
+    return res
+      .status(200)
+      .json({ success: true, data: users, message: "User list found." });
+  } catch (error) {
+    next(error);
   }
+};
 
-  async getOne(req: Request, res: Response): Promise<UserDocument[] | any> {
-    try {
-      if (!isValidObjectId(req.params.id)) {
-        res.status(404).json({ success: false, message: 'Invalid id!' });
-      }
+export const getOne = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.findById(req.params.id);
 
-      const user = await User.findById(req.params.id);
-
-      if (!user) {
-        res.status(404).json({ success: false, message: 'User not found!' });
-      }
-
-      if (user) {
-        res.status(200).json({ success: true, message: 'User found!' });
-      }
-    } catch (error) {
-      res.status(404);
+    if (!user) {
+      return next(createError(403, "User not found."));
     }
+
+    return res.status(200).json({ success: true, message: "User found." });
+  } catch (error) {
+    next(error);
   }
+};
 
-  async profile(req: Request, res: Response): Promise<UserDocument[] | any> {
-    try {
-      if (!isValidObjectId(req.body.id)) {
-        res.status(404).json({ success: false, message: 'Invalid id!' });
-      }
+export const remove = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = req?.params?.id as string;
 
-      const user = await User.findById(req?.user?._id);
+    const user = await User.findById(id);
 
-      if (!user) {
-        res.status(404).json({ success: false, message: 'User not found!' });
-      }
-
-      if (user && req.user?._id !== user?._id.toString()) {
-        res.status(404).json({ success: false, message: 'User not found!' });
-      }
-
-      user!.password = undefined;
-
-      return res.status(200).json({ success: true, data: user });
-    } catch (error) {
-      res.status(404);
+    if (!user) {
+      return next(createError(404, "User not found."));
     }
-  }
 
-  async update(req: Request, res: Response): Promise<UserDocument[] | any> {
-    try {
-      const userLogged = req.user;
-      const user = await User.findById(req.params.id);
-
-      if (!user) {
-        return res.status(404).json({ success: false, message: 'User not found.' });
-      }
-
-      if (userLogged?._id?.toString() !== user?._id?.toString()) {
-        return res.status(401).json({ success: false, message: 'Unauthorized! Access Token invalid!' });
-      }
-
-      const updateUser = new User(user);
-
-      updateUser.bio = req?.body?.bio ?? user?.bio;
-      updateUser.location = req?.body?.location ?? user?.location;
-      updateUser.role = req?.body?.role ?? user?.role;
-      updateUser.birthDate = req?.body?.birthDate ?? user?.birthDate;
-      updateUser.name = req?.body?.name ?? user?.name;
-      updateUser.image = req?.body?.image ?? user?.image;
-
-      // updateUser.password = req?.body?.password ?? user?.password;
-      // updateUser.answers = req?.body?.answers ?? user?.answers;
-
-      const newUser = await User.findByIdAndUpdate(req.params.id, updateUser, { upsert: true, returnOriginal: false });
-
-      if (!newUser) {
-        return res.status(404).json({ success: false, message: 'User not updated.' });
-      }
-
-      res.status(200).json({ success: true, data: newUser });
-    } catch (error) {
-      res.status(404);
+    if (req?.user?._id !== user?._id?.toString()) {
+      return next(createError(403, "You can delete only your account."));
     }
+
+    await User.findByIdAndDelete(id);
+
+    return res.status(200).send({
+      success: true,
+      message: "User account removed!",
+    });
+  } catch (error) {
+    next(error);
   }
+};
 
-  async delete(req: Request, res: Response): Promise<UserDocument[] | any> {
-    try {
-      const userLogged = req.user;
-      const user = await User.findById(req.params.id);
+export const profile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.findById(req?.user?._id);
 
-      if (!user) {
-        res.status(404).json({ success: false, message: 'User not found.' });
-      }
-
-      if (userLogged?.id !== user?._id.toString()) {
-        res.status(401).json({ success: false, message: 'Unauthorized! Access Token invalid!' });
-      }
-
-      const objectId = new mongoose.Types.ObjectId(req.params.id);
-      const deletedGoal = await User.findOneAndDelete(objectId);
-
-      res.status(200).json({ success: !!deletedGoal });
-    } catch (error) {
-      res.status(404);
+    if (!user) {
+      return next(createError(404, "User profile not found."));
     }
+
+    user!.password = undefined;
+
+    return res
+      .status(200)
+      .json({ success: true, data: user, message: "User profile found." });
+  } catch (error) {
+    next(error);
   }
-}
+};
+
+export const update = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return next(createError(404, "User not found."));
+    }
+
+    const updateUser = new User(user);
+
+    updateUser.bio = req?.body?.bio ?? user?.bio;
+    updateUser.location = req?.body?.location ?? user?.location;
+    updateUser.role = req?.body?.role ?? user?.role;
+    updateUser.birthDate = req?.body?.birthDate ?? user?.birthDate;
+    updateUser.name = req?.body?.name ?? user?.name;
+    updateUser.image = req?.body?.image ?? user?.image;
+
+    // updateUser.password = req?.body?.password ?? user?.password;
+    // updateUser.answers = req?.body?.answers ?? user?.answers;
+
+    const newUser = await User.findByIdAndUpdate(req.params.id, updateUser, {
+      upsert: true,
+      returnOriginal: false,
+    });
+
+    if (!newUser) {
+      return next(createError(404, "User not updated."));
+    }
+
+    return res
+      .status(200)
+      .send({ success: true, data: newUser, message: "User updated." });
+  } catch (error) {
+    next(error);
+  }
+};
