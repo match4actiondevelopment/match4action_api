fed4dbb86e48ab2eb9ec0aea99bf27488f5b8b4f
alessandrarequena <alessandra.requena@gmail.com>
2024-04-10 11:40:43 -0300

    cookie configuration

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -28,8 +28,8 @@ export const login = async (
     }
 
     return res
-      .cookie("access_token", loginDone.access_token, { httpOnly: true })
-      .cookie("refresh_token", loginDone.refresh_token, { httpOnly: true })
+      .cookie("access_token", loginDone.access_token, { httpOnly: false })
+      .cookie("refresh_token", loginDone.refresh_token, { httpOnly: false })
       .status(200)
       .send({
         data: loginDone.data,
