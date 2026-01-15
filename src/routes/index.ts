430f673ddfaa10d4972e83fb70be94fb5ee460d7
Jorge J Quintero <jjq200000@utdallas.edu>
2026-01-15 16:20:47 -0600

    Ignore .env.production

--- src/routes/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./auth";
-export * from "./goals";
-export * from "./initiatives";
-export * from "./upload";
-export * from "./users";
--- src/utils/currentDateTime.ts
+++
@@ -1,9 +0,0 @@
-export const giveCurrentDateTime = () => {
-  const today = new Date();
-  const date =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDay();
-  const time =
-    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  const dateTime = date + "-" + time;
-  return dateTime;
-};
