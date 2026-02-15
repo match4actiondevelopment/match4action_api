89ca4c038e4ea235a2c0f0476653b0a6eb00668e
ERshyama <shyama.kumari.arunachalam@gmail.com>
2026-02-15 18:22:12 -0500

    Fixing timeout issue due to cold start when connecting to MongoDB from Vercel

---
+++ src/utils/db.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import { MONGO_URI } from "./secrets";
+
+if (!MONGO_URI) {
+    throw new Error(
+        "Please define the MONGO_URI environment variable inside .env.local"
+    );
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = (global as any).mongoose;
+
+if (!cached) {
+    cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function connectToDatabase() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false, // Disable buffering to fail fast if no connection
+        };
+
+        cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
+    return cached.conn;
+}
+
+export default connectToDatabase;
